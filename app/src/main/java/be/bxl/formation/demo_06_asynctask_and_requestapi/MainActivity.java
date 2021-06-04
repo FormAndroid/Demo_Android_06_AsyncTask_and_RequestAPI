7ac9edf2b76a295987a6d20a3d7bd0dafd0d8156
Aurelien <aurelien.strimelle@bstorm.be>
2021-06-04 06:33:09 -0700

    Initial commit

---
+++ app/src/main/java/be/bxl/formation/demo_06_asynctask_and_requestapi/MainActivity.java
@@ -0,0 +1,14 @@
+package be.bxl.formation.demo_06_asynctask_and_requestapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
